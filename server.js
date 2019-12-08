fcc50b375f3be945590d0ab194b687d7e538c028
Ben Calvert <benhcalvert@gmail.com>
2019-12-08 15:06:41 -0700

    server running

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const connection = require("config/connection.js");
+const connection = require("./config/connection.js");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Parse application body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/Controller.js");
 
-app.use(routes);
+// app.use(routes);
 
 // Start server so it can begin listening to client requests.
 app.listen(PORT, function() {
